609be0df1551da2dafd396a127d411bf085cf3bd
Mehmet Cuci <mehmetcuci0@gmail.com>
2020-04-19 00:28:31 +0300

    Post işlemi tanımlandı

--- routes/movie.js
+++ routes/movie.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET movies listing. */
-router.get('/', (req, res, next) => {
-  res.json({status: 1})
+// Models
+
+const Movie = require("../models/Movie");
+
+router.post('/', (req, res, next) => {
+  const { title, imdb_score, category, country, year } = req.body;
+  const movie = new Movie({
+    title,
+    imdb_score,
+    country,
+    category,
+    year
+  })
+
+  movie.save((err, data) => {
+    if(err){
+      return res.json(err);
+    }
+    else{
+      return res.json(data);
+    }
+  })
+  
 });
 
 module.exports = router;
