800c76ea0d7c1cd616dbe30217c03c204b6d468c
Mehmet Cuci <mehmetcuci0@gmail.com>
2020-04-19 15:13:28 +0300

    Mocha & Chai testi tanımlandı

---
+++ test/index.test.js
@@ -0,0 +1,19 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const should = chai.should();
+const server = require('../app');
+
+chai.use(chaiHttp);
+
+// Testlerin ne testi olduğunu açıklıyoruz
+describe('Node Server', () => {
+    // her describe içinde birden fazla it olabilir, itler içinde istediğimiz unit test yapılabilir
+    it('(GET /) anasayfayı döndürür', (done) => {
+        chai.request(server)
+        .get('/')
+        .end((err, res) => {
+            res.should.have.status(200);
+            done();
+        })
+    })
+})
