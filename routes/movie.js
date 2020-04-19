3d2562569d5283d709062ea0b737554888a01331
Mehmet Cuci <mehmetcuci0@gmail.com>
2020-04-19 04:09:14 +0300

    movie kısmı tamamlandı

--- routes/movie.js
+++ routes/movie.js
@@ -16,6 +16,65 @@ router.get('/', (req, res, next) => {
 
 });
 
+
+router.get('/top10', (req, res) => {
+
+  const top10 = Movie.find({}).limit(10).sort({imdb_score: -1});
+  top10.then((data) => {
+    res.json(data);
+  }).catch(err => {
+    res.json(err);
+  })
+
+});
+
+
+
+router.get('/:movie_id', (req, res, next) => {
+  
+  const data = Movie.findById(req.params.movie_id);
+
+  data.then((movie) => {
+    if(!movie){
+      next({message: "Listelenecek film bulunamadı.", code: 99});
+    }
+    res.json(movie);
+  }).catch(err => {
+    res.json(err);
+  })
+
+});
+
+router.put('/:movie_id', (req, res, next) => {
+  
+  const data = Movie.findByIdAndUpdate(req.params.movie_id, req.body, {new: true});
+
+  data.then((movie) => {
+    if(!movie){
+      next({message: "Listelenecek film bulunamadı.", code: 99});
+    }
+    res.json(movie);
+  }).catch(err => {
+    res.json(err);
+  })
+
+});
+
+router.delete('/:movie_id', (req, res, next) => {
+  
+  const data = Movie.findByIdAndRemove(req.params.movie_id, req.body);
+
+  data.then((movie) => {
+    if(!movie){
+      next({message: "Listelenecek film bulunamadı.", code: 99});
+    }
+    res.json({status: 1});
+  }).catch(err => {
+    res.json(err);
+  })
+
+});
+
 router.post('/', (req, res, next) => {
   const { title, imdb_score, category, country, year } = req.body;
   const movie = new Movie({
@@ -26,15 +85,6 @@ router.post('/', (req, res, next) => {
     year
   })
 
-  /*movie.save((err, data) => {
-    if(err){
-      return res.json(err);
-    }
-    else{
-      return res.json(data);
-    }
-  })*/
-
   const addData = movie.save();
   addData.then((data) => {
     res.json({status: 1});
@@ -43,5 +93,22 @@ router.post('/', (req, res, next) => {
   })
 
 });
+
+router.get('/between/:start_year/:end_year', (req, res) => {
+
+  const { start_year, end_year } = req.params;
+  const allMovies = Movie.find(
+    {
+      year: { "$gte" : parseInt(start_year), "$lte" : parseInt(end_year) }
+    }
+  );
+  allMovies.then((data) => {
+    res.json(data);
+  }).catch(err => {
+    res.json(err);
+  })
+
+});
+
 
 module.exports = router;
